3ce08bc30afcd3465c779e76349e246f52eabd74
Aida Abdikulova <abdikulova@gmail.com>
2019-07-03 04:57:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,10 +83,6 @@ def adding_matz
      }
 programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
-
-                  :known_for => "Ruby",
-                  :languages => ["LISP", "C"]
-                  }
 puts programmer_hash
 end
 
